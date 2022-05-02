8f27ba72bd97334e24f14842c2a7a6783bb1d28e
pudges-place <53303400+pudges-place@users.noreply.github.com>
2022-05-01 18:44:41 -0600

    Add files via upload

---
+++ create-image.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+
+_check_if_root() {
+    local whiptail_installed
+
+    if [ $(id -u) -ne 0 ]
+    then
+       whiptail_installed=$(pacman -Qs libnewt)
+       if [[ "$whiptail_installed" != "" ]]; then
+          whiptail --title "Error - Cannot Continue" --msgbox "Please run this script with sudo or as root" 8 47
+          exit
+       else
+          printf "${RED}Error - Cannot Continue. Please run this script with sudo or as root.${NC}\n"
+          exit
+       fi
+    fi
+    if [[ "$SUDO_USER" == "" ]]; then
+        USERNAME=$USER
+    else
+        USERNAME=$SUDO_USER  
+    fi
+}   # end of function _check_if_root
+
+_choose_device() {
+
+   local base_dialog_content
+   local dialog_content
+   local finished
+   local exit_status
+
+   base_dialog_content="\nThe following storage devices were found\n\n$(lsblk -o NAME,MODEL,FSTYPE,SIZE,FSUSED,FSAVAIL,MOUNTPOINT)\n\n \
+   Enter source device name without a partition designation (e.g. /dev/sda or /dev/mmcblk0):"
+   dialog_content="$base_dialog_content"
+   finished=1
+   while [ $finished -ne 0 ]
+   do
+       DEVICENAME=$(whiptail --title "EndeavourOS ARM Setup - micro SD Configuration" --inputbox "$dialog_content" 27 115 3>&2 2>&1 1>&3)
+      exit_status=$?
+      if [[ "$exit_status" == "1" ]]; then           
+         printf "\nScript aborted by user\n\n"
+         exit
+      fi
+      if [[ ! -b "$DEVICENAME" ]]; then
+         dialog_content="$base_dialog_content\n    Not a listed block device, or not prefaced by /dev/ Try again."
+      else   
+         case $DEVICENAME in
+            /dev/sd*)     if [[ ${#DEVICENAME} -eq 8 ]]; then
+                             finished=0
+                          else
+                             dialog_content="$base_dialog_content\n    Input improperly formatted. Try again."   
+                          fi ;;
+            /dev/mmcblk*) if [[ ${#DEVICENAME} -eq 12 ]]; then
+                             finished=0
+                          else
+                             dialog_content="$base_dialog_content\n    Input improperly formatted. Try again."   
+                          fi ;;
+         esac
+      fi      
+   done
+}   # end of function _choose_device
+
+_unmount_partitions() {
+    local count
+    local i
+    local u
+    local x
+
+    lsblk $DEVICENAME -o MOUNTPOINT | grep /run/media > mounts
+    count=$(wc -l mounts | awk '{print $1}')
+    if [[ "$count" -gt "0" ]]
+    then
+       for ((i = 1 ; i <= $count ; i++))
+       do
+          u=$(awk -v "x=$i" 'NR==x' mounts)
+          umount $u
+       done
+       printf "\nSource Partitions unmounted\n"
+    fi
+    rm mounts
+}   # end of function _umount_partitions
+
+##################### Start of Scipt #################
+
+DEVICENAME=" "
+
+_check_if_root
+_choose_device
+mkdir MP1
+mkdir MP1/boot
+if [[ ${DEVICENAME:5:6} = "mmcblk" ]]
+then
+   DEVICENAME=$DEVICENAME"p"
+fi
+   
+PARTNAME1=$DEVICENAME"1"
+PARTNAME2=$DEVICENAME"2"
+_unmount_partitions
+
+mount $PARTNAME2 MP1
+mount $PARTNAME1 MP1/boot
+
+cd MP1
+printf "\nbsdtar is creating the image, may take a few minutes\n"
+time bsdtar -czf /home/$USERNAME/pudges-place/images/enosLinuxARM-rpi-aarch64-latest.tar.gz *
+printf "\n\nbsdtar is finished creating the image.\nand will calculate a sha512sum\n\n"
+cd ..
+sha512sum /home/$USERNAME/pudges-place/images/enosLinuxARM-rpi-aarch64-latest.tar.gz > /home/$USERNAME/pudges-place/images/enosLinuxARM-rpi-aarch64-latest.tar.gz.sha512sum
+umount MP1/boot MP1
+rm -rf MP1
+
+
+
---
+++ switch-kernel.sh
@@ -0,0 +1,121 @@
+#!/bin/bash
+
+_check_if_root() {
+    if [ $(id -u) -ne 0 ]
+    then
+      printf "\n\n${RED}PLEASE RUN THIS SCRIPT AS ROOT OR WITH SUDO${NC}\n\n"
+      exit
+    fi
+}   # end of function _check_if_root
+
+_check_internet_connection() {
+    printf "\n${CYAN}Checking Internet Connection...${NC}\n\n"
+    ping -c 3 endeavouros.com -W 5
+    if [ "$?" != "0" ]
+    then
+       printf "\n\n${RED}No Internet Connection was detected\nFix your Internet Connectin and try again${NC}\n\n"
+       exit
+    fi
+}   # end of function _check_internet_connection
+
+_find_mirrorlist() {
+    # find and install current endevouros-arm-mirrorlist
+    local tmpfile
+    local currentmirrorlist
+    local ARMARCH="aarch64"
+
+    printf "\n${CYAN}Find current endeavouros-mirrorlist...${NC}\n\n"
+    sleep 1
+    curl https://github.com/endeavouros-team/repo/tree/master/endeavouros/$ARMARCH | grep "endeavouros-mirrorlist" |sed s'/^.*endeavouros-mirrorlist/endeavouros-mirrorlist/'g | sed s'/pkg.tar.zst.*/pkg.tar.zst/'g |tail -1 > mirrors
+
+    tmpfile="mirrors"
+    read -d $'\x04' currentmirrorlist < "$tmpfile"
+
+    printf "\n${CYAN}Downloading endeavouros-mirrorlist...${NC}"
+    wget https://github.com/endeavouros-team/repo/raw/master/endeavouros/$ARMARCH/$currentmirrorlist 2>> /root/enosARM.log
+
+    printf "\n${CYAN}Installing endeavouros-mirrorlist...${NC}\n"
+    pacman -U --noconfirm $currentmirrorlist &>> /root/enosARM.log
+
+    printf "\n[endeavouros]\nSigLevel = PackageRequired\nInclude = /etc/pacman.d/endeavouros-mirrorlist\n\n" >> /etc/pacman.conf
+
+    rm mirrors
+}  # end of function _find_mirrorlist
+
+
+_find_keyring() {
+    local tmpfile
+    local currentkeyring
+    local ARMARCH="aarch64"
+
+    printf "\n${CYAN}Find current endeavouros-keyring...${NC}\n\n"
+    sleep 1
+    curl https://github.com/endeavouros-team/repo/tree/master/endeavouros/$ARMARCH |grep endeavouros-keyring |sed s'/^.*endeavouros-keyring/endeavouros-keyring/'g | sed s'/pkg.tar.zst.*/pkg.tar.zst/'g | tail -1 > keys
+
+    tmpfile="keys"
+    read -d $'\04' currentkeyring < "$tmpfile"
+
+    printf "\n${CYAN}Downloading endeavouros-keyring...${NC}"
+    wget https://github.com/endeavouros-team/repo/raw/master/endeavouros/$ARMARCH/$currentkeyring 2>> /root/enosARM.log
+
+    printf "\n${CYAN}Installing endeavouros-keyring...${NC}\n"
+    pacman -U --noconfirm $currentkeyring &>> /root/enosARM.log
+
+    rm keys
+}   # End of function _find_keyring
+
+_base_addons() {
+    ### the following installs all packages needed to match the EndeavourOS base install
+    printf "\n${CYAN}Installing EndeavourOS Base Addons...${NC}\n"
+    eos-packagelist --arch arm "Systembase + Common packages" "Firefox and language package" > base-addons
+    printf "openbox\npcmanfm-gtk3\ntint2\nnetwork-manager-applet\nxfce4-terminal\n" >> base-addons
+    pacman -S --noconfirm --needed - < base-addons
+}
+
+
+_finish_up() {
+    printf "alarm ALL=(ALL:ALL) NOPASSWD: ALL\n" >> /etc/sudoers
+    gpasswd -a alarm wheel
+    printf "exec openbox-session\n" >> /home/alarm/.xinitrc
+    chown alarm:alarm /home/alarm/.xinitrc
+    chmod 644 /home/alarm/.xinitrc
+    systemctl disable dhcpcd.service
+    systemctl enable NetworkManager.service
+    sed -i 's/ParallelDownloads = 8/#ParallelDownloads = 5/g' /etc/pacman.conf
+    pacman -Rn --noconfirm dhcpcd
+    rm /var/cache/pacman/pkg/*
+    rm switch-kernel.sh enosARM.log   
+    rm -rf /etc/pacman.d/gnupg
+    printf "\n\n${CYAN}Your uSD is ready for creating an image.\n\nPress Return to poweroff.${NC}\n"
+    read -n 1 z
+    systemctl poweroff
+}   # end of function _finish_up
+
+######################   Start of Script   #################################
+Main() {
+
+   # Declare color variables
+      GREEN='\033[0;32m'
+      RED='\033[0;31m'
+      CYAN='\033[0;36m'
+      NC='\033[0m' # No Color
+
+   # STARTS HERE
+   dmesg -n 1 # prevent low level kernel messages from appearing during the script
+   _check_if_root
+   _check_internet_connection
+   sed -i 's/#ParallelDownloads = 5/ParallelDownloads = 8/g' /etc/pacman.conf
+   pacman-key --init
+   pacman-key --populate archlinuxarm
+   pacman -Syy
+   pacman -R --noconfirm linux-aarch64 uboot-raspberrypi
+   pacman -Syu --noconfirm --needed linux-rpi raspberrypi-bootloader raspberrypi-firmware git libnewt wget networkmanager base-devel
+   _find_mirrorlist
+   _find_keyring
+   pacman -Syy
+   pacman -S --noconfirm --needed eos-packagelist
+   _base_addons
+   _finish_up
+}  # end of Main
+
+Main "$@"
