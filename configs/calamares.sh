9a8c6dc46cbfa5330fbc121508ec69edd577c416
sravanpannala <57152030+sravanpannala@users.noreply.github.com>
2022-05-05 18:35:27 -0400

    fix cleanup process

--- configs/calamares.sh
+++ configs/calamares.sh
@@ -5,5 +5,5 @@ sudo cp -f $(pwd)/packagechooser.conf /etc/calamares/modules/
 sudo cp -f $(pwd)/shellprocess_initialize_pacman.conf /etc/calamares/modules/
 sudo cp -f $(pwd)/create-pacman-keyring /etc/calamares/scripts/
 sudo cp -f $(pwd)/cleaner_script.sh /etc/calamares/scripts/
-sudo cp -f $(pwd)/eos_script_cleaner.conf /etc/calamares/modules/
+sudo cp -f $(pwd)/shellprocess_script_cleaner.conf /etc/calamares/modules/
 cd ..
