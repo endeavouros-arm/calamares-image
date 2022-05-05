f2d0c6957441f8840fa207cd39db012784c27070
sravanpannala <57152030+sravanpannala@users.noreply.github.com>
2022-05-04 21:47:19 -0400

    add pacman init to calamares

--- configs/calamares.sh
+++ configs/calamares.sh
@@ -2,4 +2,6 @@ cd calamares
 sudo cp -f $(pwd)/settings.conf  /etc/calamares/
 sudo cp -f $(pwd)/netinstall.yaml /etc/calamares/modules/
 sudo cp -f $(pwd)/packagechooser.conf /etc/calamares/modules/
+sudo cp -f $(pwd)/shellprocess_initialize_pacman.conf /etc/calamares/modules/
+sudo cp -f $(pwd)/create-pacman-keyring /etc/calamares/scripts/
 cd ..
