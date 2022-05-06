432d366b0357eb027069d5113aba80a0f34beafe
sravanpannala <57152030+sravanpannala@users.noreply.github.com>
2022-05-06 15:50:06 -0400

    remove sudo from script

--- configs/calamares.sh
+++ configs/calamares.sh
@@ -1,9 +1,9 @@
 cd calamares
-sudo cp -f $(pwd)/settings.conf  /etc/calamares/
-sudo cp -f $(pwd)/netinstall.yaml /etc/calamares/modules/
-sudo cp -f $(pwd)/packagechooser.conf /etc/calamares/modules/
-sudo cp -f $(pwd)/shellprocess_initialize_pacman.conf /etc/calamares/modules/
-sudo cp -f $(pwd)/create-pacman-keyring /etc/calamares/scripts/
-sudo cp -f $(pwd)/cleaner_script.sh /etc/calamares/scripts/
-sudo cp -f $(pwd)/shellprocess_script_cleaner.conf /etc/calamares/modules/
+cp -f $(pwd)/settings.conf  /etc/calamares/
+cp -f $(pwd)/netinstall.yaml /etc/calamares/modules/
+cp -f $(pwd)/packagechooser.conf /etc/calamares/modules/
+cp -f $(pwd)/shellprocess_initialize_pacman.conf /etc/calamares/modules/
+cp -f $(pwd)/create-pacman-keyring /etc/calamares/scripts/
+cp -f $(pwd)/cleaner_script.sh /etc/calamares/scripts/
+cp -f $(pwd)/shellprocess_script_cleaner.conf /etc/calamares/modules/
 cd ..
