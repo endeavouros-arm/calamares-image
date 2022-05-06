db0bb8baae770fbfb56335e6f12e6cb401196257
sravanpannala <57152030+sravanpannala@users.noreply.github.com>
2022-05-06 15:47:22 -0400

    add ntp, fix readme

--- config_script.sh
+++ config_script.sh
@@ -13,3 +13,6 @@ cp clean-up.service /etc/systemd/system/clean-up.service
 ./calamares.sh
 chown -R alarm .config Desktop
 chmod +x Desktop/Shutdown.sh
+printf "[Match]\nName=wlan*\n\n[Network]\nDHCP=yes\nDNSSEC=no\n" > /etc/systemd/network/wlan.network
+timedatectl set-ntp true
+timedatectl timesync-status
