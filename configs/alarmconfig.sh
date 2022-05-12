3bedbf5de4664bdb66df5db070ac8de7c008ae19
sravanpannala <57152030+sravanpannala@users.noreply.github.com>
2022-05-12 18:47:29 -0400

    fix clean-up script pacman, modify launch.sh location

--- configs/alarmconfig.sh
+++ configs/alarmconfig.sh
@@ -1,6 +1,2 @@
 cp $(pwd)/Desktop/* /home/alarm/Desktop/
 cp -r $(pwd)/.config/* /home/alarm/.config/
-cp $(pwd)/launch.sh /home/alarm/launch.sh
-cp $(pwd)/cal_online.sh /home/alarm/cal_online.sh
-cp $(pwd)/cal_ce.sh /home/alarm/cal_ce.sh
-
--- configs/clean-up.sh
+++ configs/clean-up.sh
@@ -2,7 +2,7 @@
 
 userdel -r alarm
 sed -i 's/alarm ALL=(ALL:ALL) NOPASSWD: ALL/ /g' /etc/sudoers
-pacman -Rns --noconfirm calamares_current calamares_config_default calamare_config_ce
+pacman -Rns --noconfirm calamares_current calamares_config_default calamares_config_ce
 rm -rf /etc/calamares/
 cp -f /usr/lib/systemd/system/getty@.service.bak /usr/lib/systemd/system/getty@.service
 rm -rf /usr/lib/systemd/system/getty@.service.bak
