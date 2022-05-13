f3f7e660c0fb882840ff42ce02f2e59a43b940dc
sravanpannala <57152030+sravanpannala@users.noreply.github.com>
2022-05-13 12:44:41 -0400

    change sway DM to sddm, add package removal

--- configs/clean-up.sh
+++ configs/clean-up.sh
@@ -1,5 +1,66 @@
 #!/bin/bash
 
+package=xfce4
+if pacman -Qs $package > /dev/null ; then
+  pacman -Rns openbox tint2 pcmanfm
+fi
+
+package=i3-gaps
+if pacman -Qs $package > /dev/null ; then
+  pacman -Rns openbox tint2 pcmanfm
+fi
+
+package=plasma-dekstop
+if pacman -Qs $package > /dev/null ; then
+  pacman -Rns openbox tint2 pcmanfm xfce4-terminal
+fi
+
+package=gnome-shell
+if pacman -Qs $package > /dev/null ; then
+  pacman -Rns openbox tint2 pcmanfm xfce4-terminal
+fi
+
+package=mate
+if pacman -Qs $package > /dev/null ; then
+  pacman -Rns openbox tint2 pcmanfm xfce4-terminal
+fi
+
+package=budgie-dekstop
+if pacman -Qs $package > /dev/null ; then
+  pacman -Rns openbox tint2 pcmanfm xfce4-terminal
+fi
+
+package=lxqt
+if pacman -Qs $package > /dev/null ; then
+  pacman -Rns tint2 pcmanfm xfce4-terminal
+fi
+
+package=lxde-common
+if pacman -Qs $package > /dev/null ; then
+  pacman -Rns tint2 xfce4-terminal
+fi
+
+package=bspwm
+if pacman -Qs $package > /dev/null ; then
+  pacman -Rns openbox tint2 pcmanfm
+fi
+
+package=qtile
+if pacman -Qs $package > /dev/null ; then
+  pacman -Rns openbox tint2 pcmanfm
+fi
+
+package=sway
+if pacman -Qs $package > /dev/null ; then
+  pacman -Rns openbox tint2 pcmanfm xfce4-terminal
+fi
+
+package=worm
+if pacman -Qs $package > /dev/null ; then
+  pacman -Rns tint2 pcmanfm
+fi
+
+
 userdel -r alarm
 sed -i 's/alarm ALL=(ALL:ALL) NOPASSWD: ALL/ /g' /etc/sudoers
 pacman -Rns --noconfirm calamares_current_arm calamares_config_default_arm calamares_config_ce_arm
