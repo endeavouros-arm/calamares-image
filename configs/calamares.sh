9fc1cc0d132ae360e7341ac58b5a29f00c35c77e
sravanpannala <57152030+sravanpannala@users.noreply.github.com>
2022-05-13 10:08:26 -0400

    add disable autologin module, make configs modular

--- configs/calamares.sh
+++ configs/calamares.sh
@@ -1,10 +1,6 @@
 cd calamares
+cp -f $(pwd)/modules/* /etc/calamares/modules/
+cp -f $(pwd)/scripts/* /etc/calamares/scripts/
 cp -f $(pwd)/settings_online.conf  /etc/calamares/
 cp -f $(pwd)/settings_community.conf  /etc/calamares/
-cp -f $(pwd)/netinstall.yaml /etc/calamares/modules/
-cp -f $(pwd)/netinstall-ce-base.yaml /etc/calamares/modules/
-cp -f $(pwd)/shellprocess_initialize_pacman.conf /etc/calamares/modules/
-cp -f $(pwd)/create-pacman-keyring /etc/calamares/scripts/
-cp -f $(pwd)/cleaner_script.sh /etc/calamares/scripts/
-cp -f $(pwd)/shellprocess_script_cleaner.conf /etc/calamares/modules/
 cd ..
--- configs/clean-up.sh
+++ configs/clean-up.sh
@@ -4,11 +4,7 @@ userdel -r alarm
 sed -i 's/alarm ALL=(ALL:ALL) NOPASSWD: ALL/ /g' /etc/sudoers
 pacman -Rns --noconfirm calamares_current_arm calamares_config_default_arm calamares_config_ce_arm
 rm -rf /etc/calamares/
-cp -f /usr/lib/systemd/system/getty@.service.bak /usr/lib/systemd/system/getty@.service
-rm -rf /usr/lib/systemd/system/getty@.service.bak
 rm /usr/local/bin/clean-up.sh
 rm /etc/systemd/system/clean-up.service
 
 exit
-
-
