07c9a040b73957f0b3703cb90bc3b19ab64cb45e
sravanpannala <57152030+sravanpannala@users.noreply.github.com>
2022-06-25 09:11:27 -0400

    add xed and xterm to Desktop-Base

--- switch-kernel.sh
+++ switch-kernel.sh
@@ -68,7 +68,7 @@ _base_addons() {
     ### the following installs all packages needed to match the EndeavourOS base install
     printf "\n${CYAN}Installing EndeavourOS Base Addons...${NC}\n"
     eos-packagelist --arch arm "Desktop-Base + Common packages" "Firefox and language package" > base-addons
-    printf "openbox\npcmanfm-gtk3\ntint2\nnetwork-manager-applet\nxfce4-terminal\nxed\nflameshot" >> base-addons
+    printf "openbox\npcmanfm-gtk3\ntint2\nnetwork-manager-applet\nxfce4-terminal\nflameshot" >> base-addons
     pacman -S --noconfirm --needed - < base-addons
     ### Install Calamares Arm
     pacman -U --noconfirm /home/alarm/configs/xkeyboard-config-2.35.1-1-any.pkg.tar.xz
