55c3908b954d8dd7b58e24f8d25f3c8d0bc7f711
sravanpannala <57152030+sravanpannala@users.noreply.github.com>
2022-08-24 09:40:25 -0400

    change calamares log name

--- switch-kernel-local.sh
+++ switch-kernel-local.sh
@@ -115,7 +115,7 @@ _switch_mirrors_local() {
 
 _switch_mirrors_back() {
     sed -i 's|#Server = http://mirror.archlinuxarm.org/$arch/$repo|Server = http://mirror.archlinuxarm.org/$arch/$repo|g' /etc/pacman.d/mirrorlist
-    sed -i 's|Server = http://127.0.01:21212||g' /etc/pacman.d/mirrorlist
+    sed -i 's|Server = http://127.0.0.1:21212||g' /etc/pacman.d/mirrorlist
     sed -i 's|Server = http://10.42.0.1:9129/repo/archlinux_$arch/$repo||g' /etc/pacman.d/mirrorlist
     sed -i 's|Server = http://10.42.0.1:9129/repo/endeavouros/$repo/$arch||g' /etc/pacman.d/endeavouros-mirrorlist
     # sed -i 's|Server = https://github.com/endeavouros-arm/repo/raw/master/$repo/$arch||g' /etc/pacman.d/endeavouros-mirrorlist
@@ -180,6 +180,7 @@ Main() {
    _switch_mirrors_back
 
    _finish_up
+   cat /etc/enosARM.log
 }  # end of Main
 
 Main "$@"
